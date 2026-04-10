313124cb52d9469f031329448d2eb62e80fc3f5f
Ricardo <rickhoward0811@gmail.com>
2026-04-10 10:32:52 -0300

    Filtro de categoria Adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { itens } from "./data/data.js";
+import { useState } from "react";
 import CardItem from "./assets/componentes/cardItem";
 
 function App() {
+  const [categoriaSelecionada, setCategoria] = useState("Todos")
   return (
     <div>
-      <h1>Galeria de Itens</h1>
-
-      {itens.map((item) => (
-        <CardItem key={item.id} item={item} />
-      ))}
+      <button onClick={() => setCategoria("Todos")}>Todos</button>
+      <button onClick={() => setCategoria("Arma")}>Armas</button>
+      <button onClick={() => setCategoria("Feitico")}>Feitiços</button>
+      <button onClick={() => setCategoria("Encantamento")}>Encantamentos</button>
       
+      <h1>Galeria de Itens</h1>
+      {itens.filter((item) => 
+        categoriaSelecionada === "Todos" || item.categoria === categoriaSelecionada
+      ).map((item) => (
+        <CardItem key={item.id} item={item}/>
+     ))}
     </div>
   );
 }
