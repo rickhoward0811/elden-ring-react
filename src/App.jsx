e437c47ee1848ad6388ea125459113b7dfbbf870
Ricardo <rickhoward0811@gmail.com>
2026-04-15 18:07:46 -0300

    Input para pesquisa de itens em tempo real adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import { itens } from "./data/data.js";
 import { useState } from "react";
-import CardItem from "./assets/componentes/cardItem";
-import './App.css';
-
+import CardItem from "./assets/cardItem.jsx";
+import "./App.css";
 
 function App() {
-  const [categoriaSelecionada, setCategoria] = useState("Todos")
+  const [categoriaSelecionada, setCategoria] = useState("Todos");
+  const [busca, setBusca] = useState("");
+
   return (
     <div>
+      <div className="cabecalho">
+        <header className="header">
+          <div className="logo">
+            <img src="src/assets/logo.png" alt="Logo" />
+          </div>
+          <div className="titulo">
+            <h1>EldenWiki</h1>
+          </div>
+          <div className="busca">
+            <input
+              type="text"
+              placeholder="Buscar..."
+              value={busca}
+              onChange={(e) => setBusca(e.target.value)}
+            />
+            <button>Buscar</button>
+          </div>
+        </header>
+      </div>
       <button onClick={() => setCategoria("Todos")}>Todos</button>
       <button onClick={() => setCategoria("Arma")}>Armas</button>
       <button onClick={() => setCategoria("Feitico")}>Feitiços</button>
       <button onClick={() => setCategoria("Encantamento")}>Encantamentos</button>
-            
+
       <h1>Galeria de Itens</h1>
-      {itens.filter((item) => 
-        categoriaSelecionada === "Todos" || item.categoria === categoriaSelecionada
-      ).map((item) => (
-        <CardItem key={item.id} item={item}/>
-     ))}
+      <div className="itens-container">
+        {itens
+          .filter(
+            (item) =>
+              (categoriaSelecionada === "Todos" ||
+                item.categoria === categoriaSelecionada) &&
+              item.nome.toLowerCase().includes(busca.toLowerCase()),
+          )
+          .map((item) => (
+            <CardItem key={item.id} item={item} />
+          ))}
+      </div>
     </div>
   );
 }
